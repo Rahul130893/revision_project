af2f46898ef476544c5237e83dcc2f97b4074bc5
Rahul <rahulrdx1308@gmail.com>
2022-07-21 01:13:11 +0530

    some corrections

--- back_end/src/controllers/cart.controller.js
+++ back_end/src/controllers/cart.controller.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Cart = require("../models/product.model");
+const Cart = require("../models/cart.model");
 const router = express.Router();
 
 router.get("", async (req, res) => {
