3f740e7eb9699c5493a3b0ee7da892c77ae66ca9
Rahul <rahulrdx1308@gmail.com>
2022-07-21 03:12:06 +0530

    authentication signup login

--- back_end/src/index.js
+++ back_end/src/index.js
@@ -4,18 +4,26 @@ const express = require("express")
 const connect = require("./configs/db")
 const productController= require("./controllers/product.controller")
 const cartController =require("./controllers/cart.controller")
+const userController = require("./controllers/user.controller")
+const { register, login } = require("./controllers/auth.controller");
+
 const app = express()
  
 app.use(express.json())
 app.use("/product", productController)
 app.use("/cart", cartController)
+app.use("/users", userController)
+app.post("/register", register);
+
+app.post("/login", login);
 
 
 app.listen(5000, async () => {
     try {
         await connect()
+         console.log("listening on 5000");
     } catch (error) {
         console.log(error)
     }
-    console.log("listening on 5000")
+   
 })
