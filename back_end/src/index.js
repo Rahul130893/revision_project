536721459a63d9192d5e4572c217a5d52ec700f8
Rahul <rahulrdx1308@gmail.com>
2022-07-24 23:58:00 +0530

    final correction in port

--- back_end/src/index.js
+++ back_end/src/index.js
@@ -1,14 +1,16 @@
 //const { application } = require("express")
 const express = require("express")
+const cors = require("cors")
 
 const connect = require("./configs/db")
 const productController= require("./controllers/product.controller")
 const cartController =require("./controllers/cart.controller")
 const userController = require("./controllers/user.controller")
-const { register, login } = require("./controllers/auth.controller");
-
+const { register, login } = require("./controllers/auth.controller")
 const app = express()
- 
+const dotenv = require("dotenv")
+dotenv.config()
+ app.use(cors())
 app.use(express.json())
 app.use("/product", productController)
 app.use("/cart", cartController)
@@ -17,11 +19,11 @@ app.post("/register", register);
 
 app.post("/login", login);
 
-
-app.listen(5000, async () => {
+const port=process.env.PORT || 5000
+app.listen(port, async () => {
     try {
         await connect()
-         console.log("listening on 5000");
+         console.log(`listening on ${port}`);
     } catch (error) {
         console.log(error)
     }
